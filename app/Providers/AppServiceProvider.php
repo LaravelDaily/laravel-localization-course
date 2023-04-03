27e8eab5d9f676775bf74dd108e51747c1cb93ac
modestasv <info@modestasv.lt>
2023-04-03 10:29:59 +0300

    Localizing Carbon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setLocale(app()->getLocale());
     }
 }
