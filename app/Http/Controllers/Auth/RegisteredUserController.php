0a34f88769ec1ac2d96ba75fedd75c16be7dc5de
modestasv <info@modestasv.lt>
2023-04-07 16:21:59 +0300

    Added UI Language switcher that saves language preferences into database OR session

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'language' => session('locale', config('app.locale')),
         ]);
 
         event(new Registered($user));
